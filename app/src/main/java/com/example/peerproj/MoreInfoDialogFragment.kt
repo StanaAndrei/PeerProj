01b4a37b03e63fa0ecf005f62c8ef7d1f594af1d
StanaAndrei <stadey33@gmail.com>
2022-11-04 21:07:35 +0200

    finished dialog

--- app/src/main/java/com/example/peerproj/MoreInfoDialogFragment.kt
+++ app/src/main/java/com/example/peerproj/MoreInfoDialogFragment.kt
@@ -2,16 +2,31 @@ package com.example.peerproj
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 
 class MoreInfoDialogFragment : DialogFragment() {
     private lateinit var builder: AlertDialog.Builder
 
+    companion object {
+        private const val TARGET_URL = "http://www.moma.org"
+    }
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         super.onCreateDialog(savedInstanceState)
         builder = AlertDialog.Builder(activity)
         builder.setView(R.layout.fragment_more_info_dialog)
+        builder.setPositiveButton(R.string.more_info_poz) { _, _ ->
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(TARGET_URL)
+            startActivity(intent)
+        }
+        builder.setNegativeButton(R.string.more_info_neg) { dialog, _ ->
+            dialog.cancel()
+        }
         return builder.create()
     }
 
